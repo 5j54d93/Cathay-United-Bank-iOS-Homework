6327fba0d28e31491bf67ffb299cb8d0311f0f7a
Ricky Chuang <songsongyyohya@gmail.com>
2023-02-13 09:24:33 +0800

    Initial Commit

---
+++ Cathay-United-Bank-iOS-Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cathay-United-Bank-iOS-Homework
+//
+//  Created by 莊智凱 on 2023/2/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
